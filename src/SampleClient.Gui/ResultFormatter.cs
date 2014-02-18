08dff4793b558f6d542b90cde64e504bc955806d
Tomaz Vizintin <tomaz.vizintin@gmail.com>
2014-02-18 15:55:20 +0100

    removed unused code

--- src/SampleClient.Gui/ResultFormatter.cs
+++ src/SampleClient.Gui/ResultFormatter.cs
@@ -10,13 +10,6 @@ namespace Finkit.ManicTime.Server.SampleClient.Ui
 {
     public static class ResultFormatter
     {
-        public static string FormatAsJson(object value)
-        {
-            if (value == null)
-                return null;
-            return JsonConvert.SerializeObject(value, Formatting.Indented);
-        }
-
         public static string FormatJson(string value)
         {
             if (value == null)
@@ -50,22 +43,6 @@ namespace Finkit.ManicTime.Server.SampleClient.Ui
                 }
             }
             return value;
-        }
-
-        public static string FormatAsXml(object value)
-        {
-            if (value == null)
-                return null;
-
-            var sb = new StringBuilder();
-            using (XmlWriter writer = XmlWriter.Create(sb, new XmlWriterSettings() { OmitXmlDeclaration = true, Indent = true }))
-            {
-                var noNamespaces = new XmlSerializerNamespaces();
-                noNamespaces.Add("", "");
-                var serializer = new XmlSerializer(value.GetType());
-                serializer.Serialize(writer, value, noNamespaces);
-            }
-            return sb.ToString();
         }
     }
 }
